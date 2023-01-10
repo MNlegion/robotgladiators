c25bdb915a4e24b49a5d40374ac450043709092f
MNlegion <rkreuser30@gmail.com>
2023-01-09 20:45:18 -0500

    adding conditional statements to the fight sequence

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,45 +11,45 @@ var enemyAttack = 12;
 var fight = function () {
   window.alert("Welcome to Robot Gladiators");
 
-  // subtract the value of player attack from enemy health and then use the result to update the enemy health variable
-  enemyHealth = enemyHealth - playerAttack;
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-  // log a resulting message to the console showing updated enemy health after attack
+  // if player choses to fight, then fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
+  // remove enemy's health by subtracting the amount set in the playerAttack variable
+  enemyHealth = enemyHealth - playerAttack;
   console.log(
-    playerName +
-      " attacked " +
-      enemyName +
-      ". " +
-      enemyName +
-      " now has " +
-      enemyHealth +
-      " health remaining."
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
 
+  // check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health remaining!");
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-  // subtract the value of enemy attack from player health and then use the result to update the player health variable
+
+  // remove player's health by subtracting the amount set in the enemyAttack variable
   playerHealth = playerHealth - enemyAttack;
-  // log a resulting message to the console showing updated player health after attack
   console.log(
-    enemyName +
-      " attacked " +
-      playerName +
-      ". " +
-      playerName +
-      " now has " +
-      playerHealth +
-      " health remaining."
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
+  // check player's health
   if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
   } else {
-    window.alert(enemyName + " still has" + playerHealth + " health remaining!");
+    window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-};
+  // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+  window.alert(playerName + " has chosen to skip the fight!");
+} else {
+  window.alert("You need to choose a valid option. Try again!");
+}
+
+}
 
 fight();
+  
+
+
