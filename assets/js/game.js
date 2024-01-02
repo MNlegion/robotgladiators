8fc39ea4fdb42b483fe92bb54a50e7b17ffa59ec
MNlegion <rkreuser30@gmail.com>
2024-01-01 20:08:07 -0500

    intial round of fight logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,13 +5,29 @@ var playerAttack = 10;
 // Use math.random to compare enemy attack to player defense and subtract from player health
 
 var enemyName = "Roborto";
-var enemyHealth = 50;
+var enemyHealth = 10;
 var enemyAttack = 12;
 // var enemyDefense = Math.floor(Math.random() * 10) + 1;
 // Use math.random to compare player attack to enemy defense and subtract from enemy health
 
 var fight = function() {
   alert("Welcome to Robot Gladiators! " + playerName + " vs. " + enemyName +"! " + playerName + ", are you ready to rumble!?");
-}
+
+  enemyHealth = enemyHealth - playerAttack;
+  if (enemyHealth <= 0) {
+    alert(enemyName + " has died!");
+  } else {console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  )};
+
+  playerHealth = playerHealth - enemyAttack;
+  if (playerHealth <= 0) {
+    alert(playerName + " has died!");
+  } else {
+    console.log(
+      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    )
+  };
+};
 
 fight();
