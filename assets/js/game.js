b6ee56db3c794c9695752c0df7d30575e87d9a8f
MNlegion <rkreuser30@gmail.com>
2024-01-01 19:52:55 -0500

    player and enemy initialization

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,17 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+// var playerDefense = Math.floor(Math.random() * 10) + 1;
+// Use math.random to compare enemy attack to player defense and subtract from player health
 
-console.log(playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+// var enemyDefense = Math.floor(Math.random() * 10) + 1;
+// Use math.random to compare player attack to enemy defense and subtract from enemy health
 
-function fight() {
-  alert("The fight has begun! " + playerName + ", are you ready to rumble?")
+var fight = function() {
+  alert("Welcome to Robot Gladiators! " + playerName + " vs. " + enemyName +"! " + playerName + ", are you ready to rumble!?");
 }
 
-// fight()
+fight();
