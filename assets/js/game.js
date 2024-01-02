38f757890153898e5932a475ae666353fb87c0b2
MNlegion <rkreuser30@gmail.com>
2024-01-01 19:45:52 -0500

    continued intial setup

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+function fight() {
+  alert("The fight has begun! " + playerName + ", are you ready to rumble?")
+}
+
+// fight()
